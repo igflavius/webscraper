a805735251f8e64624777ccfe0b7153850ef56ad
Flavius Ion <igflavius@odyssee.ro>
2020-06-26 00:15:35 +0300

    added comments to read the code more easily

--- web.py
+++ web.py
@@ -12,6 +12,10 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 def arguments():
+    """
+        This is the argument() function
+        Obviously these are the arguments.
+    """    
     parser = argparse.ArgumentParser(prog='web.py', description='Web Scraper v1.0')
     parser.add_argument("-i", "--ip", dest="ip", help='ip list file', required=True)
     parser.add_argument("-u", "--uri", dest="uri", help='uri list file', required=True)
@@ -23,6 +27,10 @@ def arguments():
     return arg
 
 def main():
+    """
+        This is where we begin. We create a nested loop 
+        with itertools and we  multithreaded the list.
+    """
     threads = []
     num_threads = arg.num_threads
 
@@ -37,6 +45,11 @@ def main():
             uri = uri.rstrip().strip()
             threads_queue.put(url + uri)
 
+    """
+        This is the first part to brake the loop.
+        We add None to the list. To brake the loop in 
+        the scanner() function with an if statement.
+    """ 
     for thread in threads:
         threads_queue.put(None)
 
@@ -44,7 +57,10 @@ def main():
         thread.join()
 
 def scanner():
-    
+    """ 
+        In the scanner() function we get url from the queue.
+        and find the string in the urls.
+    """    
     logging.basicConfig(format='%(message)s', level=logging.INFO, handlers=[logging.FileHandler(arg.log), logging.StreamHandler(sys.stdout)])
 
     headers = requests.utils.default_headers()
@@ -53,12 +69,17 @@ def scanner():
     match = ["It works!", "Test", "Univention Portal", "RouterOS router configuration page"]
 
     while True:
-
+        # Get urls from the Queue()
         url = threads_queue.get()
 
+        """
+            This is the second part to break the loop.
+            I'll try to find a better way to try brake the loop.
+        """        
         if url is None:
             break
         
+        # request the url and find the match
         try:
             req = requests.get(url, headers=headers, timeout=3, allow_redirects=True, verify=False)
             soup = BS(req.text, 'html.parser')
@@ -69,7 +90,6 @@ def scanner():
             pass
 
 if __name__ == '__main__':
-    
     threads_queue = queue.Queue(maxsize=0)
     arg = arguments()
     main()
