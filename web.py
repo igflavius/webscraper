0389bc6b8071fc689e2ba12e0e6a005fe503476b
Flavius Ion <igflavius@odyssee.ro>
2020-06-26 15:19:38 +0300

    we add the functionality to the program: port, ssl

--- web.py
+++ web.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+__author__ = "Flavius Ion"
+__email__ = "igflavius@odyssee.ro"
+__license__ = "MIT"
+__version__ = "1.0"
+
 import argparse
 import requests
 import logging
@@ -13,7 +18,6 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 def arguments():
     """
-        This is the argument() function
         Obviously these are the arguments.
     """    
     parser = argparse.ArgumentParser(prog='web.py', description='Web Scraper v1.0')
@@ -21,19 +25,21 @@ def arguments():
     parser.add_argument("-u", "--uri", dest="uri", help='uri list file', required=True)
     parser.add_argument("-l", "--log", dest="log", help="log to save results", required=True)
     parser.add_argument("-p", "--port", dest="port", type=int, default=80, help="port number (default 80)")
-    parser.add_argument("-s", "--ssl", dest="ssl", help="use ssl or no")
     parser.add_argument("-t", "--threads", dest="num_threads", type=int, default=50, help="number of threads (default 50)")
+    parser.add_argument("-s", "--ssl", dest="ssl", action="store_true", help="use ssl or no")
     arg = parser.parse_args()
     return arg
 
 def main():
     """
         This is where we begin. We create a nested loop 
-        with itertools and we  multithreaded the list.
+        with itertools and we put in the queue() list.
     """
     threads = []
     num_threads = arg.num_threads
+    port = str(arg.port)
 
+    # Multithread
     for i in range(num_threads):
         th = threading.Thread(target = scanner, daemon=True)
         threads.append(th)
@@ -43,11 +49,11 @@ def main():
         for url, uri in itertools.product(f1, f2):
             url = url.rstrip().strip()
             uri = uri.rstrip().strip()
-            threads_queue.put(url + uri)
+            threads_queue.put(url + ":" + port + uri)
 
     """
-        This is the first part to brake the loop.
-        We add None to the list. To brake the loop in 
+        This is the first part to break the loop.
+        We add None to the list. To break the loop in 
         the scanner() function with an if statement.
     """ 
     for thread in threads:
@@ -66,28 +72,38 @@ def scanner():
     headers = requests.utils.default_headers()
     headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36'})
 
-    match = ["It works!", "Test", "Univention Portal", "RouterOS router configuration page"]
+    match = ["It works!", "Test", "Univention Portal", "RouterOS router configuration page", "UniFi Network"]
 
     while True:
-        # Get urls from the Queue()
         url = threads_queue.get()
+        ssl = arg.ssl
 
         """
             This is the second part to break the loop.
-            I'll try to find a better way to try brake the loop.
+            I'll try to find a better way to try break the loop.
         """        
         if url is None:
             break
-        
-        # request the url and find the match
-        try:
-            req = requests.get(url, headers=headers, timeout=3, allow_redirects=True, verify=False)
-            soup = BS(req.text, 'html.parser')
+        if ssl is False:         
+            try:
+                req = requests.get("http://" + url, headers=headers, timeout=3, allow_redirects=True, verify=False)
+                soup = BS(req.text, 'html.parser')
 
-            if soup.find_all(string=match):
-                logging.info(url)
-        except requests.RequestException as err:
-            pass
+                if soup.find_all(string=match):
+                    logging.info("http://" + url)
+            except requests.RequestException as err:
+                pass
+        else:
+            try:
+                req = requests.get("https://" + url, headers=headers, timeout=3, allow_redirects=True, verify=False)
+                soup = BS(req.text, 'html.parser')
+
+                if soup.find_all(string=match):
+                    logging.info("https://" + url)
+            except requests.RequestException as err:
+                pass
+        
+
 
 if __name__ == '__main__':
     threads_queue = queue.Queue(maxsize=0)
